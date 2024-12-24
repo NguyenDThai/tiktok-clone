b28b51444656306be05fbb765218345cbe862d75
NguyenDThai <thainguyen4646@gmail.com>
2024-12-24 20:25:42 +0700

    setup bo cuc giao dien

--- src/components/layout/DefaulLayout/index.js
+++ src/components/layout/DefaulLayout/index.js
@@ -1,13 +1,16 @@
 import Header from '~/components/layout/components/Header';
 import SideBar from './SideBar';
+import styles from './DefaulLayout.module.scss';
+import classNames from 'classnames/bind';
 
+const cx = classNames.bind(styles);
 function DefaulLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <SideBar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );

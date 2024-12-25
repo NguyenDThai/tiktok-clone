7740061e9ca9797be197a783c6b92a31127f2042
NguyenDThai <thainguyen4646@gmail.com>
2024-12-25 10:05:18 +0700

    Header ui logo and search

--- src/components/layout/components/Header/index.js
+++ src/components/layout/components/Header/index.js
@@ -1,3 +1,7 @@
+import images from '~/assets/image';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 
@@ -5,7 +9,20 @@ const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Search account and video" spellCheck={false} />
+                    <button className={cx('clear-btn')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }

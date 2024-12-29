55eb88a838c5197055d4720fd56d612e23a93648
NguyenDThai <thainguyen4646@gmail.com>
2024-12-29 08:16:10 +0700

    tach com image va icon

---
+++ src/components/Image/index.js
@@ -0,0 +1,25 @@
+import { useState, forwardRef } from 'react';
+import images from '~/assets/image';
+import classNames from 'classnames';
+import styles from './Image.module.scss';
+
+const Image = forwardRef(({ src, alt, className, fallBack: customFallback = images.noImage, ...props }, ref) => {
+    const [fallback, setFallback] = useState('');
+
+    const handleError = () => {
+        setFallback(customFallback);
+    };
+
+    return (
+        <img
+            className={classNames(styles.wrapper, className)}
+            ref={ref}
+            src={fallback || src}
+            alt={alt}
+            {...props}
+            onError={handleError}
+        />
+    );
+});
+
+export default Image;
--- src/components/layout/components/Header/index.js
+++ src/components/layout/components/Header/index.js
@@ -4,6 +4,8 @@ import Button from '~/components/Button';
 import AccountItems from '~/components/AccountItem';
 import Menu from '~/components/Popper/Menu';
 import styles from './Header.module.scss';
+import { MessageIcon, NotifaceIcon, UploadIcon } from '~/components/Icons';
+import Image from '~/components/Image';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -20,7 +22,6 @@ import {
     faUser,
 } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
-import { IoCloudUploadOutline } from 'react-icons/io5';
 import Tippy from '@tippyjs/react';
 import HeadLessTippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
@@ -122,7 +123,20 @@ function Header() {
                         <>
                             <Tippy delay={[0, 200]} content="Upload video" placement="bottom">
                                 <button className={cx('action-btn')}>
-                                    <IoCloudUploadOutline />
+                                    <UploadIcon />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 200]} content="Tin nhan" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <MessageIcon />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 200]} content="Hop thu" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <div className={cx('notiface-btn')}>
+                                        <NotifaceIcon />
+                                        <span className={cx('notiface-num')}>11</span>
+                                    </div>
                                 </button>
                             </Tippy>
                         </>
@@ -135,10 +149,11 @@ function Header() {
 
                     <Menu item={MenuUser ? MenuUser : MENU_ITEMS}>
                         {currentUser ? (
-                            <img
+                            <Image
                                 className={cx('user-avatar')}
                                 src="https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-1/367032458_311220494748976_1189553963787691400_n.jpg?stp=cp0_dst-jpg_s40x40_tt6&_nc_cat=110&ccb=1-7&_nc_sid=e99d92&_nc_ohc=ujW5K2fWfYAQ7kNvgHsOIrD&_nc_zt=24&_nc_ht=scontent.fsgn5-10.fna&_nc_gid=AEZVvZjgLVzbLFPbQi8ehEz&oh=00_AYA5jmO-5I_M6EX4unXFCkz2Uk_qDQUI37zMSqx01PQgvQ&oe=6775345C"
                                 alt="Nguyen van a"
+                                fallBack="https://fullstack.edu.vn/assets/f8-icon-lV2rGpF0.png"
                             />
                         ) : (
                             <button className={cx('more-btn')}>
